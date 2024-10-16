4c70abf8f9d3376e7a69cfed43d3211e46f90997
uczen <uczen@zs10.pl>
2024-10-16 13:24:53 +0200

    dzialanie 3 przyciskow

--- app/src/main/java/pl/zabrze/zs10/quiz/MainActivity.java
+++ app/src/main/java/pl/zabrze/zs10/quiz/MainActivity.java
@@ -3,16 +3,84 @@ package pl.zabrze.zs10.quiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private ArrayList<Pytanie> listaPytan = new ArrayList<>();
+    private TextView textView;
+    private int nrPytania;
+    private Button nastepny;
+    private Button tak;
+    private Button nie;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        wypelnijPytania();
+        textView = findViewById(R.id.textViewTrescPytania);
+        nrPytania = 0;
+        wstawPytanie(nrPytania);
+        nastepny = findViewById(R.id.button4);
+        tak = findViewById(R.id.button);
+        nie = findViewById(R.id.button2);
+        nastepny.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        nrPytania++;
+                        if(nrPytania == listaPytan.size()){
+                            nastepny.setVisibility(View.INVISIBLE);
+                            tak.setVisibility(View.INVISIBLE);
+                            nie.setVisibility(View.INVISIBLE);
+                            textView.setText("Zdobyłeś: "+ policzPuntky() +" na " + listaPytan.size());
+                        }else{
+                            wstawPytanie(nrPytania);
+                        }
+                    }
+                }
+        );
+        tak.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        sprawdzToPytanie(true);
+                    }
+                }
+        );
+        nie.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        sprawdzToPytanie(false);
+                    }
+                }
+        );
+    }
+    private int policzPuntky(){
+        int ilePunktow = 0;
+        for (Pytanie pytanie:listaPytan) {
+            if(pytanie.isUdzielnoPoprawnaOdpowiedz()){
+                ilePunktow++;
+            }
+        }
+        return ilePunktow;
+    }
+    private void sprawdzToPytanie(boolean odpowiedzUzytkownika){
+        listaPytan.get(nrPytania).setUdzielnoPoprawnaOdpowiedz(false);
+        if(listaPytan.get(nrPytania).isPoprawna() == odpowiedzUzytkownika){
+            listaPytan.get(nrPytania).setUdzielnoPoprawnaOdpowiedz(true);
+        }
+    }
+    private void wstawPytanie(int i){
+        Pytanie pytanie = listaPytan.get(i);
+        textView.setText(pytanie.getTresc());
     }
     private void wypelnijPytania(){
         listaPytan.add(new Pytanie(
